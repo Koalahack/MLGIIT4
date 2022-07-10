b9ce2c18f1ea03fce95ed2d1a81bfe7cd56b2539
Koalahack <cristian.paz@correounivalle.edu.co>
2022-07-10 17:31:55 -0500

    codigo total

--- calculos.R
+++ calculos.R
@@ -1,19 +1,59 @@
 # 1. Paquete #####
 
-library(haven)
 library(MPV)
 library(car)
 library(MASS)
-library(lmtest)
+library(pROC)
 
 library(ggplot2)
 library(gridExtra)
 library(plotly)
-library(readr)
 
 # 2. Datos ####
 
 data =  read.csv("AbusoDrogas.txt", sep="")
+
+data = data[,c(2,3,5,7,9)]
+head(data)
+
+## 2.1. Visualización de datos ####
+
+
+
+# 3. Modelo logístico ####
+
+# Enlace Logit
+mlg1.logit = glm(DFREE~AGE + BECK + NDRUGTX + TREAT,family = binomial("logit"),data)
+summary(mlg1.logit)
+
+## 3.1. Modelo logístico con interacciones ####
+
+# Enlace Logit e interacciones (punto2)
+mlg2.logit = glm(DFREE~AGE + BECK + NDRUGTX +AGE*TREAT + BECK*TREAT + NDRUGTX*TREAT + TREAT,family = binomial("logit"),data)
+summary(mlg2.logit)
+
+## 3.2. Modelos con otras funciones ####
+
+# (punto3)
+
+# Enlace probit
+mlg3.probit = glm(DFREE~AGE + BECK + NDRUGTX + TREAT,family = binomial("probit"),data)
+summary(mlg3.probit)
+
+# Enlace log complementaria
+mlg4.logcom = glm(DFREE~AGE + BECK + NDRUGTX + TREAT,family = binomial("cloglog"),data)
+summary(mlg4.logcom)
+
+# Comparación de con curva rog
+
+# curvas roc
+ROCbw.logit = roc(data$DFREE~mlg1.logit$fitted.values)
+ROCbw.probit = roc(data$DFREE~mlg3.probit$fitted.values)
+ROCbw.cloglog = roc(data$DFREE~mlg4.logcom$fitted.values)
+plot(ROCbw.logit)
+lines(ROCbw.probit,col=2)
+lines(ROCbw.cloglog,col=3)
+
 
 
 
