866a7a0799728f373eb8a2047cd898423ac3b367
Koalahack <cristian.paz@correounivalle.edu.co>
2022-07-10 10:27:21 -0500

    codigo ejemplo

--- calculos.R
+++ calculos.R
@@ -1,2 +1,19 @@
+# 1. Paquete #####
+
+library(haven)
+library(MPV)
+library(car)
+library(MASS)
+library(lmtest)
+
+library(ggplot2)
+library(gridExtra)
+library(plotly)
+library(readr)
+
+# 2. Datos ####
+
+data =  read.csv("AbusoDrogas.txt", sep="")
+
 
 
