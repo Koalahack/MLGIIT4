caa4794b9334ca60120b66df7ecefc41041ed687
Koalahack <cristian.paz@correounivalle.edu.co>
2022-07-11 22:15:22 -0500

    calculos

--- calculos.R
+++ calculos.R
@@ -26,11 +26,17 @@ head(data)
 mlg1.logit = glm(DFREE~AGE + BECK + NDRUGTX + TREAT,family = binomial("logit"),data)
 summary(mlg1.logit)
 
+# Interpretación de los coeficientes
+exp(mlg1.logit$coefficients)
+
 ## 3.1. Modelo logístico con interacciones ####
 
 # Enlace Logit e interacciones (punto2)
 mlg2.logit = glm(DFREE~AGE + BECK + NDRUGTX +AGE*TREAT + BECK*TREAT + NDRUGTX*TREAT + TREAT,family = binomial("logit"),data)
 summary(mlg2.logit)
+
+# Interpretación de los modelos
+exp(mlg2.logit$coefficients)
 
 ## 3.2. Modelos con otras funciones ####
 
@@ -44,15 +50,21 @@ summary(mlg3.probit)
 mlg4.logcom = glm(DFREE~AGE + BECK + NDRUGTX + TREAT,family = binomial("cloglog"),data)
 summary(mlg4.logcom)
 
+# Enlace cauchit
+mlg5.cauchit = glm(DFREE~AGE + BECK + NDRUGTX + TREAT,family = binomial("cauchit"),data)
+summary(mlg5.cauchit)
+
 # Comparación de con curva rog
 
 # curvas roc
 ROCbw.logit = roc(data$DFREE~mlg1.logit$fitted.values)
 ROCbw.probit = roc(data$DFREE~mlg3.probit$fitted.values)
 ROCbw.cloglog = roc(data$DFREE~mlg4.logcom$fitted.values)
+ROCbw.cauchit = roc(data$DFREE~mlg5.cauchit$fitted.values)
 plot(ROCbw.logit)
 lines(ROCbw.probit,col=2)
 lines(ROCbw.cloglog,col=3)
+lines(ROCbw.cauchit,col=4)
 
 
 
